cf5a9213280acb40639bb1bcb7c84d1c679c2446
Emilio-Rdz <78563761+Emilio-Rdz@users.noreply.github.com>
2021-02-18 19:34:53 -0600

    commit graxias

--- cereal.py
+++ cereal.py
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-input ("Hola que tal este es programa que calcula el peso de una caja de cereal en toneladas metricas")
-input ("Y calcula el numero de cajas necesarias para una tonelada")
-peso = int(input("ingrese el peso del cereal en onzas: "))
-cajas = 35273.92 / peso
-tons = peso / 35273.92
-print ("Una caja de cerel pesa", tons, "toneladas metricas")
-print ("Una tonelada metrica son", cajas, "cajas de cereal")
-
-=======
 input ("programa que calcula el peso de una caja de cereal en toneladas metricas")
 input ("Y calcula el numero de cajas necesarias para una tonelada")
 peso = int(input("ingrese el peso del cereal en onzas: "))
@@ -15,5 +5,4 @@ cajas = 35273.92 / peso
 tons = peso / 35273.92
 print ("Una caja de cerel pesa", tons, "toneladas metricas")
 print ("Una tonelada metrica son", cajas, "cajas de cereal")
-print("Gracias")
->>>>>>> 0a43eb135e48356e76eff78061ec183e8fefbca7
+print("Graxias")
