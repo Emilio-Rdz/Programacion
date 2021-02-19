c7c20f5b34266cff5a26d0563e32862500c40baa
Emilio-Rdz <78563761+Emilio-Rdz@users.noreply.github.com>
2021-02-18 18:00:12 -0600

    Commit inicial

---
+++ cereal.py
@@ -0,0 +1,7 @@
+input ("programa que calcula el peso de una caja de cereal en toneladas metricas")
+input ("Y calcula el numero de cajas necesarias para una tonelada")
+peso = int(input("ingrese el peso del cereal en onzas: "))
+cajas = 35273.92 / peso
+tons = peso / 35273.92
+print ("Una caja de cerel pesa", tons, "toneladas metricas")
+print ("Una tonelada metrica son", cajas, "cajas de cereal")
